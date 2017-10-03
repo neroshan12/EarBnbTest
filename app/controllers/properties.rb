947bcce2af96215b4335d8a2496a25bf984266f2
Marcus Rands <mrands@hotmail.co.uk>
2017-10-03 16:11:38 +0100

    Added functionality for adding and viewing properties

--- app/controllers/properties.rb
+++ app/controllers/properties.rb
@@ -5,11 +5,12 @@ class EarBnb < Sinatra::Base
 
   get '/properties' do
     @properties = Property.all
+    @message = session['message']
     erb :'properties/index'
   end
 
   post '/properties' do
-    @property = current_user.propertys.new(description: params[:description])
+    @property = current_user.propertys.new(description: params[:description],:address1 => params[:address1])
     if @property.save
       current_user.save
       redirect '/properties'
@@ -17,9 +18,25 @@ class EarBnb < Sinatra::Base
       flash.now[:errors] = @property.errors.full_messages
       erb :'properties/new'
     end
+    # property = Property.create(
+    # :address1 => params[:address1],
+    # :price_per_night => params[:price],
+    # :description => params[:description]
+    # )
+    # session['message'] = params[:address1]
+    # redirect to('/properties')
   end
 
   get '/properties/new' do
     erb :'properties/new'
   end
+
+  get '/property/:id' do
+    @property = Property.get(params['id'])
+    # @property.address1
+    # @comments = Comment.all(:property_id => params['id'])
+    erb(:'properties/property')
+
+  end
+
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,16 +1,25 @@
 class Property
   include DataMapper::Resource
 
-  belongs_to :user
+  #belongs_to :user
 
-  property :id,           Serial
-  property :description,    Text, required: true,
+  property :id,               Serial
+  property :description,      Text, required: true,
                           length: 1..100,
                           messages: {
                             presence: "Description must not be empty",
                             length: "Description must not be more than 100 characters"
                           }
-  property :created_at,   DateTime
+  property :address1,         String
+  property :address2,         String
+  property :address3,         String
+  property :city,             String
+  property :state,            String
+  property :post_code,        String
+  property :price_per_night,  Integer
+  property :bedrooms,         Integer
+  property :image_url,        String
+  property :created_at,       DateTime
 
   def format_time(time = @created_at)
     "#{time.strftime("%R")} on #{time.strftime("%d/%-m")}"
